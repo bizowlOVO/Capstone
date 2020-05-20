e1305c84f6cb609f9d50530694aa90b4bef7fd72
anono <anono@DESKTOP-NKL4A50>
2020-05-20 13:54:18 +0900

    tester

---
+++ movierecommender/src/test/java/kr/ac/sunmoon/movierecommender/MovierecommenderApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.sunmoon.movierecommender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovierecommenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
