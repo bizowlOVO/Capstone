1e77ee8532782002411e63027501f1473025f314
anono <anono@DESKTOP-NKL4A50>
2020-05-21 00:53:03 +0900

    # WARNING: head commit changed in the meantime
    tester

---
+++ movierecommender/src/main/java/kr/ac/sunmoon/movierecommender/tester/TesterController.java
@@ -0,0 +1,14 @@
+package kr.ac.sunmoon.movierecommender.tester;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TesterController {
+	
+	@GetMapping(value = "/")
+	public String tester() {
+		System.out.println("jsp 호출 성공");
+		return "index";
+	}
+}
