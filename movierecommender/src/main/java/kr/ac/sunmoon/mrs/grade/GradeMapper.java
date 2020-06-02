8afeafd6a518af87a5ddcc4d56dd48fad69086a2
bizowl <bizowlOVO@gmail.com>
2020-06-03 01:04:21 +0900

    Grade Dir & Mapper Interface Added

---
+++ movierecommender/src/main/java/kr/ac/sunmoon/mrs/grade/GradeMapper.java
@@ -0,0 +1,8 @@
+package kr.ac.sunmoon.mrs.grade;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface GradeMapper {
+
+}
--- movierecommender/src/main/java/kr/ac/sunmoon/mrs/movie/MovieController.java
+++ movierecommender/src/main/java/kr/ac/sunmoon/mrs/movie/MovieController.java
@@ -1,5 +1,8 @@
 package kr.ac.sunmoon.mrs.movie;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class MovieController {
 
 }
