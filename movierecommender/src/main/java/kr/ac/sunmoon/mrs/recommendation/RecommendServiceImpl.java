cabdb144c3dffef814a74378b4a0a3dff58889bf
KSOLIP <KSOLIP@DESKTOP-SF53FTA>
2020-06-27 15:01:46 +0900

    메인 페이지 추천 개발 중

--- movierecommender/src/main/java/kr/ac/sunmoon/mrs/recommendation/RecommendServiceImpl.java
+++ movierecommender/src/main/java/kr/ac/sunmoon/mrs/recommendation/RecommendServiceImpl.java
@@ -7,31 +7,22 @@ import org.springframework.stereotype.Service;
 
 import kr.ac.sunmoon.mrs.agent.Movie;
 import kr.ac.sunmoon.mrs.movie.MovieMapper;
-import kr.ac.sunmoon.mrs.review.ReviewMapper;
 
 @Service
-public class RecommendServiceImpl {
-	/*
-	 * @Autowired private MovieMapper movieMapper;
-	 * 
-	 * @Autowired private ReviewMapper reviewMapper;
-	 * 
-	 * public List<Movie> inquiryRankRecentlyList() {//영화 최근 등록 순 List<Movie> result
-	 * = movieMapper.selectRankRecently();
-	 * 
-	 * return result; }
-	 * 
-	 * public List<Movie> inquiryRankViewList() {//영화 조회수 순 List<Movie> result =
-	 * movieMapper.selectRankView();
-	 * 
-	 * return result; }
-	 * 
-	 * public List<Movie> inquiryRankReviewList() {//영화 리뷰 순 List<Movie> movie =
-	 * reviewMapper.selectRankReview();
-	 * 
-	 * List<Movie> result = null; for (int i = 0; i < movie.size(); i++) { result =
-	 * (List<Movie>) movieMapper.selectMovie(movie.get(i)); }
-	 * 
-	 * return result; }
-	 */
+public class RecommendServiceImpl implements RecommendService {
+	@Autowired
+	private MovieMapper movieMapper;
+	
+	public List<Movie> inquiryRankRecentlyList() {
+		return movieMapper.selectRankRecently();
+	}
+
+	public List<Movie> inquiryRankViewList() {
+		return movieMapper.selectRankView();
+	}
+
+	public List<Movie> inquiryRankReviewList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
